fcaa50d5662b879dce352abfa3e1b8eceed8343b
KEHONG CHEN <k.chen2.22@abdn.ac.uk>
2023-05-01 19:45:11 +0000

    add .venv and db

--- Shopping_app/views.py
+++ Shopping_app/views.py
@@ -2,27 +2,26 @@ from django.shortcuts import render
 from .models import Volcano
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
-'''
-def store(request):
-    # 获取所有的 Volcano 对象
-    volcanoes = Volcano.objects.all()
-    # 将 volcanoes 对象传递到模板中
-    context = {'volcanoes': volcanoes}
-    return render(request, 'store/store.html', context)
-'''
+from django.db.models import Q
+#ignore warning of query
+import warnings
+from django.core.paginator import UnorderedObjectListWarning
+
+warnings.simplefilter('ignore', UnorderedObjectListWarning)
+
 
 def store(request):
-    # 获取所有的 Volcano 对象
+    query = request.GET.get('query')
     volcanoes = Volcano.objects.all()
-    
-    # 使用 Paginator 对象实现分页
+    if query:
+        volcanoes = Volcano.objects.filter(Q(Volcano_Name__icontains=query) | Q(Country__icontains=query) | Q(Volcano_Type__icontains=query)).order_by('-Volcano_ID')
     paginator = Paginator(volcanoes, 15)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    
-    # 将 page_obj 对象传递到模板中
+
     context = {'page_obj': page_obj}
     return render(request, 'store/store.html', context)
+
 
 
 def cart(request):
