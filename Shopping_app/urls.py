184a4eeb5a5819b4f598139d4356160bd6b7704d
KEHONG CHEN <k.chen2.22@abdn.ac.uk>
2023-05-05 17:02:06 +0000

    fix cart

--- Shopping_app/urls.py
+++ Shopping_app/urls.py
@@ -10,6 +10,4 @@ urlpatterns = [
     path('cart/remove/<int:cart_item_id>/', views.remove_cart_item, name='remove_cart_item'),
     path('place_order/', views.place_order, name='place_order'),
     path('create_empty_cart/', views.create_empty_cart, name='create_empty_cart'),
-    path('volcano_list/', views.volcano_list, name='volcano_list'),
-
 ]
